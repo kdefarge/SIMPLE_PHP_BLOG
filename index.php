a666adf5e3fe90b985140dd2d6b0d3e03296b67f
kdefarge <1819360+kdefarge@users.noreply.github.com>
2020-04-30 19:36:40 +0200

    Add index router for load twig templates add base template add home template add admin template add twig cache to .gitignore

--- index.php
+++ index.php
@@ -8,4 +8,28 @@
 
 require "vendor/autoload.php";
 
+define("DEV", true);
+
+$loader = new \Twig\Loader\FilesystemLoader('app/templates');
+
+$twig = new \Twig\Environment($loader, [
+    'debug' => DEV,
+    'cache' => 'app/cache'
+]);
+
+if(isset($_GET['page']))
+{
+    switch($_GET['page']) {
+        case "admin" :
+            echo $twig->render('admin.html');
+            break;
+        default:
+            echo $twig->render('home.html');
+    }
+}
+else
+{
+    echo $twig->render('home.html');
+}
+
 ?>
