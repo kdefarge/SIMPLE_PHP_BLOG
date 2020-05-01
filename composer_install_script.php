84081e665da34eefe61df1b863c89b447fd46e3a
kdefarge <1819360+kdefarge@users.noreply.github.com>
2020-05-01 16:47:51 +0200

    add jquery file with personal composer post script

--- composer_install_script.php
+++ composer_install_script.php
@@ -1,6 +1,9 @@
 <?php
 
-$mkdirs = [
+define("JS",1);
+define("CSS",2);
+
+$dirs = [
     "public",
     "public/script",
     "public/css"
@@ -17,7 +20,7 @@ $files =  [
     ]
 ];
 
-foreach($mkdirs as $dir) {
+foreach($dirs as $dir) {
 
     if(!is_dir($dir)) {
         mkdir($dir);
@@ -26,10 +29,10 @@ foreach($mkdirs as $dir) {
 
 }
 
-foreach($files as $file_name => $dirs ) {
+foreach($files as $file_name => $paths ) {
 
-    $source = $dirs[0].$file_name;
-    $dest = $dirs[1].$file_name;
+    $source = $paths[0].$file_name;
+    $dest = $paths[1].$file_name;
 
     if(is_file($dest)) {
         unlink($dest);
@@ -40,5 +43,25 @@ foreach($files as $file_name => $dirs ) {
     echo('copy : '.$source."\r\n".'in : '.$dest."\r\n");
     
 }
+
+$files_jquery = "https://github.com/jquery/jquery/archive/3.5.0.zip";
+$tmp_file = "tmp.zip";
+$file_name = "jquery.min.js";
+$file_path = "jquery-3.5.0/dist/";
+
+file_put_contents($tmp_file, fopen("$files_jquery", 'r'));
+echo('dowloaded : '.$files_jquery."\r\n");
+
+$zip = new ZipArchive;
+if ($zip->open($tmp_file) === TRUE) {
+    $file_content = $zip->getFromName($file_path.$file_name);
+    file_put_contents($dirs[JS].'/'.$file_name, $file_content);
+    $zip->close();
+} else {
+    echo 'échec';
+}
+
+unlink($tmp_file);
+echo('deleted '.$tmp_file."\r\n");
 
 ?>
